a5a47467dc81944dc0c0b21d4b22600182418a1e
trevormccrt <trevormccrt@gmail.com>
2023-02-13 13:53:04 -0500

    update cuda speedtest

--- scripts/cuda_speedtest.py
+++ scripts/cuda_speedtest.py
@@ -1,46 +1,59 @@
 import cupy as cp
+from cupyx.profiler import benchmark
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import time
 
-from boolean_networks import binary_core, cuda_binary_core
+import binary_core
 
-function_dimension = 3
-batch_sizes = 3 * np.floor(np.logspace(start=3, stop=8, num=15)).astype(np.int64)
+os.environ["CUPY_ACCELERATORS"] = "cutensor"
+
+function_dimension = 8
+batch_sizes = 1 * np.floor(np.logspace(start=3, stop=8, num=15)).astype(np.int64)
 all_times_numpy = []
 all_times_cupy = []
 dtype = np.bool_
 
-data_unit = binary_core.random_binary_data((1, function_dimension), 0.5).astype(dtype)
-function_unit = binary_core.random_binary_data((1, 1<<function_dimension), 0.5).astype(dtype)
 
+data_unit = np.random.randint(0, 2, (1, function_dimension)).astype(dtype)
+function_unit = np.random.randint(0, 2, (1, 1<<function_dimension)).astype(dtype)
 for i, batch_size in enumerate(batch_sizes):
     print(batch_size)
-    data = np.random.randint(0, 2, (batch_size, function_dimension)).astype(dtype)#np.tile(data_unit, (batch_size, 1))
-    functions = np.random.randint(0, 2, (batch_size, 1<<function_dimension)).astype(dtype)#np.tile(function_unit, (batch_size, 1))
-    data_cp = cp.array(data)
-    functions_cp = cp.array(functions)
-    if not i:
-        cuda_binary_core.apply_binary_function(data_cp, functions_cp)
+    try:
+        data = np.tile(data_unit, (batch_size, 1))
+        functions = np.tile(function_unit, (batch_size, 1))
+    except:
+        print("batch generation failed")
+        break
     print("starting numpy")
-    numpy_start_time = time.time()
-    _ = binary_core.apply_binary_function(data, functions)
-    numpy_end_time = time.time()
-    print("numpy time: {}".format(numpy_end_time - numpy_start_time))
+    try:
+        data_cp = cp.array(data)
+        functions_cp = cp.array(functions)
+        times = benchmark(binary_core.apply_binary_function, (data, functions), n_repeat=10)
+        np_time = np.mean(times.cpu_times) + np.mean(times.gpu_times)
+        print("numpy time: {}".format(np_time))
+        all_times_numpy.append(np_time)
+    except:
+        print("numpy failed")
     print("starting cupy")
-    cp_start_time = time.time()
-    _ = cuda_binary_core.apply_binary_function(data_cp, functions_cp)
-    cp_end_time = time.time()
-    print("cupy time: {}".format(cp_end_time - cp_start_time))
-    all_times_numpy.append(numpy_end_time - numpy_start_time)
-    all_times_cupy.append(cp_end_time - cp_start_time)
+    try:
+        data_cp = cp.array(data)
+        functions_cp = cp.array(functions)
+        times = benchmark(binary_core.apply_binary_function, (data_cp, functions_cp), n_repeat=10)
+        cp_time = np.mean(times.cpu_times) + np.mean(times.gpu_times)
+        print("cupy time: {}".format(cp_time))
+        all_times_cupy.append(cp_time)
+    except:
+        print("cupy failed")
+
+
     print("\n")
 
 fig, axs = plt.subplots()
-axs.plot(batch_sizes, all_times_numpy, label="numpy")
-axs.plot(batch_sizes, all_times_cupy, label="cuda")
+axs.plot(batch_sizes[:len(all_times_numpy)], all_times_numpy, label="numpy")
+axs.plot(batch_sizes[:len(all_times_cupy)], all_times_cupy, label="cuda")
 axs.legend()
 axs.set_xscale("log")
 axs.set_yscale("log")
