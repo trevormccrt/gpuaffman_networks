764cc638961716ce73997a85e374c24d53bb79b3
trevormccrt <trevormccrt@gmail.com>
2023-02-17 17:33:15 -0500

    parralel scripts ready

--- genetics/task_runners/shared_and_gpu_batch.py
+++ genetics/task_runners/shared_and_gpu_batch.py
@@ -10,7 +10,7 @@ N = 12
 population_size = 70
 keep_best = int(0.8 * population_size)
 n_children = population_size - keep_best
-n_populations = 200
+n_populations = 70
 n_trajectories = 20
 noise_prob = 0.01
 mutation_rate = 0.001
@@ -18,7 +18,7 @@ mutation_rate = 0.001
 init_avg_k = 2
 max_k = 3
 
-n_generations = 400000
+n_generations = 300000
 n_memory_timesteps = 10
 
 
@@ -27,12 +27,9 @@ os.makedirs(out_dir, exist_ok=False)
 checkpointint_dir = os.path.join(out_dir, "checkpoint_data/")
 os.makedirs(checkpointint_dir, exist_ok=False)
 
-dev0 = cp.cuda.Device(0)
-
-
 file_name = "batch_1.npz"
-with dev0:
-    evolution_runners.evolve_batch(N, max_k, population_size, keep_best, n_populations, n_trajectories, noise_prob,
-                                   mutation_rate, init_avg_k, n_generations, n_memory_timesteps,
-                                   tasks.make_3_bit_input_state, tasks.evaluate_shared_and_task, out_dir, file_name,
-                                   True, checkpointing_dir=checkpointint_dir, checkpointing_freq=1000)
+
+evolution_runners.evolve_batch(N, max_k, population_size, keep_best, n_populations, n_trajectories, noise_prob,
+                               mutation_rate, init_avg_k, n_generations, n_memory_timesteps,
+                               tasks.make_3_bit_input_state, tasks.evaluate_shared_and_task, out_dir, file_name,
+                               True, checkpointing_dir=checkpointint_dir, checkpointing_freq=1000)
