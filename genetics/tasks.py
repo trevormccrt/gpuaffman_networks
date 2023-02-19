e45a2c4942a36041b29bbee866c8a6be576028ed
trevormccrt <trevormccrt@gmail.com>
2023-02-19 17:07:59 -0500

    work

--- genetics/tasks.py
+++ genetics/tasks.py
@@ -5,6 +5,7 @@ import binary_core
 
 _3_BIT_TT = binary_core.truth_table_columns(3)
 _4_BIT_TT = binary_core.truth_table_columns(4)
+_6_BIT_TT = binary_core.truth_table_columns(6)
 
 _PAND_DESIRED_OUT = np.stack([np.bitwise_and(_4_BIT_TT[:, 0], _4_BIT_TT[:, 1]),
                               np.bitwise_and(_4_BIT_TT[:, 2], _4_BIT_TT[:, 3])])
@@ -15,11 +16,19 @@ _SHARED_AND_DESIRED_OUT = np.stack([np.bitwise_and(_3_BIT_TT[:, 0], _3_BIT_TT[:,
 _SEQUENTIAL_AND_DESIERD_OUT = np.stack([np.bitwise_and(_3_BIT_TT[:, 0], _3_BIT_TT[:, 1]),
                                         np.bitwise_and(np.bitwise_and(_3_BIT_TT[:, 0], _3_BIT_TT[:, 1]), _3_BIT_TT[:, 2])])
 
+_MODULAR_6_DESIRED_OUT = np.bitwise_or(np.bitwise_and(np.bitwise_or(_6_BIT_TT[:, 0], _6_BIT_TT[:, 1]),
+                                                      np.bitwise_or(_6_BIT_TT[:, 2], _6_BIT_TT[:, 3])),
+                                       np.bitwise_and(_6_BIT_TT[:, 4], _6_BIT_TT[:, 5]))
+
+print("")
+
+
 try:
     import cupy as cp
     _PAND_DESIRED_OUT_GPU = cp.array(_PAND_DESIRED_OUT)
     _SHARED_AND_DESIRED_OUT_GPU = cp.array(_SHARED_AND_DESIRED_OUT)
     _SEQUENTIAL_AND_DESIERD_OUT_GPU = cp.array(_SEQUENTIAL_AND_DESIERD_OUT)
+    _MODULAR_6_DESIRED_OUT_GPU = cp.array(_MODULAR_6_DESIRED_OUT)
 
 except:
     pass
@@ -62,6 +71,12 @@ def make_4_bit_input_state(N):
     return input_state
 
 
+def make_6_bit_input_state(N):
+    input_state = np.zeros((16, N), dtype=np.uint8)
+    input_state[:, :6] = _6_BIT_TT
+    return input_state
+
+
 def evaluate_pnand_task(data):
     data = np.moveaxis(data, 1, -2)
     desired_out = _PAND_DESIRED_OUT
@@ -86,6 +101,15 @@ def evaluate_sequential_and_task(data):
     if not isinstance(data, np.ndarray):
         desired_out = _SEQUENTIAL_AND_DESIERD_OUT_GPU
     error_rate = np.mean(np.equal(data[:, :, :, :, (3, 4)], desired_out.T), axis=(0, -2, -1))
+    return error_rate
+
+
+def evaluate_modular_6_task(data):
+    data = np.moveaxis(data, 1, -2)
+    desired_out = _MODULAR_6_DESIRED_OUT
+    if not isinstance(data, np.ndarray):
+        desired_out = _MODULAR_6_DESIRED_OUT_GPU
+    error_rate = np.mean(np.equal(data[:, :, :, :, 6], desired_out.T), axis=(0, -2, -1))
     return error_rate
 
 
