37614123c310dc582043d49055b68e0769413f7e
Trevor McCourt <trevormccourt@Trevors-MacBook-Pro.local>
2023-02-27 03:22:22 -0500

    work

--- genetics/task_runners/and_gpu_batch.py
+++ genetics/task_runners/and_gpu_batch.py
@@ -9,7 +9,7 @@ N = 8
 population_size = 70
 keep_best = int(0.4 * population_size)
 n_children = population_size - keep_best
-n_populations = 20
+n_populations = 40
 n_trajectories = 200
 noise_prob = 0.01
 mutation_rate = 0.005
@@ -17,17 +17,20 @@ mutation_rate = 0.005
 init_avg_k = 2
 max_k = 3
 
-n_generations = 200
+n_generations = 50000
 n_memory_timesteps = 10
 
 a = os.getenv("DATA_DIR")
 out_dir = os.path.join(a, "boolean_network_data/and_evolution_results/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
 os.makedirs(out_dir, exist_ok=False)
+checkpointint_dir = os.path.join(out_dir, "checkpoint_data/")
+os.makedirs(checkpointint_dir, exist_ok=False)
 
 
 file_name = "batch_1.npz"
 
 f_breed = lambda f, c, uc : ragged_task_evolution.graph_crossover_breed(f, c, uc, [0, 1, 2])
+#f_breed = ragged_task_evolution.pair_breed_swap_all
 
 f_mutate = lambda f, c, uc: ragged_task_evolution.mutate_equal_prob(f, c, uc, mutation_rate)
 
@@ -35,4 +38,5 @@ evolution_runners.evolve_batch(N, max_k, population_size, keep_best, n_populatio
                                mutation_rate, init_avg_k, n_generations, n_memory_timesteps,
                                tasks.make_2_bit_input_state, tasks.evaluate_and_task,
                                f_breed, f_mutate ,
-                               out_dir, file_name, using_cuda=True, checkpointing_freq=10)
+                               out_dir, file_name, using_cuda=True,
+                               checkpointing_freq=100, checkpointing_dir=checkpointint_dir)
