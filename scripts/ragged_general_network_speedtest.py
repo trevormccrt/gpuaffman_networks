40c0480127b34b4166bbc46ed0c6f60b35948b05
trevormccrt <trevormccrt@gmail.com>
2023-03-08 13:36:16 -0500

    profiling of genetic algo

--- scripts/ragged_general_network_speedtest.py
+++ scripts/ragged_general_network_speedtest.py
@@ -7,23 +7,18 @@ import os
 
 import ragged_general_network, general_network
 
-os.environ["CUPY_ACCELERATORS"] = "cutensor"
-
-function_dimension = 3
-N = 8
-max_k = 8
-init_avg_k = 2
-batch_sizes = 3 * np.floor(np.logspace(start=3, stop=8, num=15)).astype(np.int64)
+N = 25
+max_k = 4
+init_avg_k = 3
+batch_sizes = 2 * np.floor(np.logspace(start=3, stop=6, num=15)).astype(np.int64)
 all_times = []
 for batch_size in batch_sizes:
     try:
-        states = cp.random.randint(0, 2, (batch_size, N), dtype=cp.uint8).astype(cp.bool_)
-        functions = cp.random.randint(0, 2, (batch_size, N, 1 << max_k), dtype=cp.uint8).astype(cp.bool_)
-        connectivity = cp.random.randint(0, N, (batch_size, N, max_k), dtype=cp.uint8)
-        #used_connectivity = cp.random.binomial(1, init_avg_k/max_k, (batch_size, N, max_k), dtype=cp.bool_)
-
-        #times = benchmark(ragged_general_network.ragged_k_state_update, (states, functions, connectivity, used_connectivity), n_repeat=10)
-        times = benchmark(general_network.state_update, (states, functions, connectivity), n_repeat=10)
+        times = benchmark(ragged_general_network.ragged_k_state_update,
+                          (cp.random.randint(0, 2, (batch_size, N), dtype=cp.uint8).astype(cp.bool_),
+                           cp.random.randint(0, 2, (batch_size, N, 1 << max_k), dtype=cp.uint8).astype(cp.bool_),
+                           cp.random.randint(0, N, (batch_size, N, max_k), dtype=cp.uint8),
+                           cp.random.binomial(1, init_avg_k/max_k, (batch_size, N, max_k), dtype=cp.bool_)), n_repeat=1)
         all_times.append(np.mean(times.cpu_times) + np.mean(times.gpu_times))
     except cupy.cuda.memory.OutOfMemoryError:
         print("failed {}".format(batch_size))
